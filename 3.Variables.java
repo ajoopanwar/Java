ba425e53f306c9e6a4ed6b97fc014e2abe74ecfc
ajoopanwar <pooja@Poojas-MacBook-Air.local>
2023-01-19 23:40:24 +0530

    variables

---
+++ 3.Variables.java
@@ -0,0 +1,15 @@
+class Variables{
+    public static void main(String a[])
+    {
+        System.out.println(3 + 5);
+
+        int num1 = 5;
+        int num2 = 10;
+        System.out.println(num1 + num2);
+
+        int num3 = 10;
+        int num4 = 10;
+        int res = num3 + num4;
+        System.out.println(res);
+    }
+}
