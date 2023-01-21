b0c67f560eed9e32252ff0e54a0d21a8c01ad103
ajoopanwar <pooja@Poojas-MacBook-Air.local>
2023-01-21 23:26:28 +0530

    literals in java

---
+++ 5.Literal.java
@@ -0,0 +1,21 @@
+class Literals{
+    public static void main(String args[]){
+        
+        int a = 45;
+        System.out.println(a);
+
+        a = 0b101;
+        System.out.println(a);
+
+        a = 0x7e;
+        System.out.println(a);
+
+        a = 1_111_00_00;
+        System.out.println(a);
+
+        char c = 'a';
+        System.out.println(++c);
+
+
+    }
+}
