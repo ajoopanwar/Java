362ac43b7a704d256bfbac6c10ec99217b1360ec
ajoopanwar <pooja@Poojas-MacBook-Air.local>
2023-01-22 00:44:02 +0530

    Assignment operator

---
+++ 7.AssignmentOperators.java
@@ -0,0 +1,18 @@
+class AssignmentOperators{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        
+        int c = a+b;
+        System.out.println(c);
+
+        c = a-b;
+        System.out.println(c);
+
+        c = a++;
+        System.out.println(c);
+
+        c = ++a;
+        System.out.println(c);
+    }
+}
