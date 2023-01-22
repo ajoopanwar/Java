c1e52aea7d5678eded1d27fbd0ea8b99698db944
ajoopanwar <pooja@Poojas-MacBook-Air.local>
2023-01-22 13:17:15 +0530

    bitwise operator

--- 8.RelationalOperators.java
+++ 8.RelationalOperators.java
@@ -11,5 +11,15 @@ class RelationalOperators{
 
         res = a <= b;
         System.out.println(res);
+        
+        //bitwise operators
+        int x = 2;
+        System.out.println(~(x));
+        System.out.println(5 & 3);
+        System.out.println(5 | 3);
+        System.out.println(5 ^ 3);
+        System.out.println(5 >> 1);
+        System.out.println(5 << 1);
+        System.out.println(5 >>> 1);
     }
 }
