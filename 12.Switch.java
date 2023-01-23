9ab6064660043edca20c8c1fe03dd1170c6c0c39
ajoopanwar <pooja@Poojas-MacBook-Air.local>
2023-01-24 00:18:09 +0530

    switch-statements

---
+++ 12.Switch.java
@@ -0,0 +1,16 @@
+class Switch{
+    public static void main(String args[]){
+        int n = 10;
+        
+        switch(n){
+            case 1: System.out.println("Sunday");
+                    break;
+            case 2: System.out.println("Monday");
+                    break;
+            case 3: System.out.println("Tuesday");
+                    break;
+            default: System.out.println("hello");
+        }
+
+    }
+}
