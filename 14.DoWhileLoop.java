7059e813145c13a8d10e6bcbaa8b446b609a0405
ajoopanwar <pooja@Poojas-MacBook-Air.local>
2023-01-24 00:29:33 +0530

    do while loop

---
+++ 14.DoWhileLoop.java
@@ -0,0 +1,11 @@
+class DoWhileLoop{
+    public static void main(String args[]){
+        int i = 0;
+
+        do{
+            System.out.println("hello "+i);
+            i++;
+        }
+        while(i<0);
+    }
+}
