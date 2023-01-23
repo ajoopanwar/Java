4ba12f2113b0f250a151ceb8830629d4f4feb5fc
ajoopanwar <pooja@Poojas-MacBook-Air.local>
2023-01-24 00:48:23 +0530

    for loop

---
+++ 15.forLoop.java
@@ -0,0 +1,12 @@
+class ForLoop{
+    public static void main(String args[]){
+        for(int i = 0;i<5;i++){
+            System.out.println("Inside for loop i "+i);
+        }
+
+        int j = 0;
+        for(;j<5;j++){
+            System.out.println("Inside for loop j "+j);
+        }
+    }
+}
