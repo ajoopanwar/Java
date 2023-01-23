175edb546e09605b023630c0a2224ef0fd116f79
ajoopanwar <pooja@Poojas-MacBook-Air.local>
2023-01-24 00:25:58 +0530

    while loop

---
+++ 13.WhileLoop.java
@@ -0,0 +1,10 @@
+class WhileLoop{
+    public static void main(String args[]){
+        int n = 2;
+
+        while(n < 5){
+            System.out.println("Hello");
+            n++;
+        }
+    }
+}
