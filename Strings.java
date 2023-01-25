0c5e415197b9b9c16bf766223f57740fa71c3b66
ajoopanwar <pooja.panwar.rke@gmail.com>
2023-01-25 22:30:12 +0530

    Strings in java

---
+++ Strings.java
@@ -0,0 +1,8 @@
+public class Strings{
+    public static void main(String args[]){
+        String str = new String("Pooja");
+
+        System.out.println("Hello " + str);
+        System.out.println(str.concat(" Panwar"));
+    }
+}
