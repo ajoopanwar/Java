d74c842115cb9207bfa28a08010dc9225abe073c
ajoopanwar <pooja.panwar.rke@gmail.com>
2023-01-30 00:03:39 +0530

    multilevel inheritance

---
+++ MultilevelInheritance.java
@@ -0,0 +1,11 @@
+public class MultilevelInheritance{
+    public static void main(String args[]){
+    AdvUltraC advuC = new AdvUltraC();
+
+    System.out.println(advuC.add(2,2));
+    System.out.println(advuC.sub(2,2));
+    System.out.println(advuC.mul(2,2));
+    System.out.println(advuC.div(2,2));
+    System.out.println(advuC.power(2,2));
+    }
+}
