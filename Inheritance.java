fe7f2665baa6a81af1e72e3cb682df15c3c940dc
ajoopanwar <pooja.panwar.rke@gmail.com>
2023-01-29 23:52:24 +0530

    inheritance

---
+++ Inheritance.java
@@ -0,0 +1,10 @@
+public class Inheritance{
+    public static void main(String args[]){
+        AdvC advc = new AdvC();
+
+        System.out.println(advc.add(2,2));
+        System.out.println(advc.sub(2,2));
+        System.out.println(advc.mul(2,2));
+        System.out.println(advc.div(2,2));
+    }
+}
